0003c1339d0288e30f8cff1914345199b601051a
ahmedelagmey <ahmedelajmey@gmail.com>
2022-12-21 20:31:06 +0200

    final project

--- udagram-frontend/src/environments/environment.ts
+++ udagram-frontend/src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   appName: 'Udagram',
-  apiHost: 'http://localhost:8080/api/v0'
+  apiHost: 'http://ae08ed82b48b74191a599aaf8f8335e6-1734009246.us-east-1.elb.amazonaws.com:8080/api/v0'
 };
 
 /*
